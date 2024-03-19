11c307fbf4603a200ca84c18b328dc3341f96fe1
JSSatorres <juansataz.dev@gmail.com>
2024-03-19 20:06:11 +0100

    feat: inital components

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "../store/counter";
+import { defineProps, defineSlots, defineModel, defineOptions } from "vue";
+defineProps<{ msg: string }>();
+
+const { count } = storeToRefs(useCounterStore());
+const filePath = ref(null);
+const openFile = async () => {
+  //@ts-ignore
+  filePath.value = await window.electronAPI.openFile();
+};
+
+
