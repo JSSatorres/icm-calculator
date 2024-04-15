1053737e1987cc96d88378a40c64d53227cb2b95
JSSatorres <juansataz.dev@gmail.com>
2024-04-15 21:36:40 +0200

    feat: table component initial version

---
+++ src/components/TableComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "../store/counter";
+import { defineProps } from "vue";
+defineProps<{ msg: string }>();
+
+const { count } = storeToRefs(useCounterStore());
+const filePath = ref(null);
+const openFile = async () => {
+  //@ts-ignore
+  filePath.value = await window.electronAPI.openFile();
+};
+</script>
+<template>
+  <div style="background-color: beige">
+    <h1>Home Page</h1>
+    <img :src="imageUrl" alt="home logo" width="100" height="100" />
+  </div>
+</template>
